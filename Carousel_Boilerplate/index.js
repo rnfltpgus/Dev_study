55be1bac96230c16fce9f5f4b60090ec43953025
rnfltpgus <80088956+rnfltpgus@users.noreply.github.com>
2021-12-26 20:55:07 +0900

    슬라이드 여백 조정

--- Carousel_Boilerplate/index.js
+++ Carousel_Boilerplate/index.js
@@ -1,7 +1,7 @@
 new Swiper('.promotion .swiper-container', {
   // direction: 'horizontal' // 방향, 수평(horizontal) 하지만 swier에서 제공하는 기본값이 수평이라서 명시안해도됨
   slidesPerView: 3, // 한번에 보여줄 슬라이드 개수
-  spaceBetween: 20, // 슬라이드 사이 여백
+  spaceBetween: 10, // 슬라이드 사이 여백
   centeredSlides: true, // 1번 슬라이드가 가운데 보이기
   loop: true, // 반복재생여부
   autoplay: {
